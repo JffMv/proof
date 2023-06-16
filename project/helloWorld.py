5c6c250f266e7ba825d69c9cfe35e94d51dd6e89
Jefferson Mesa <mesajefer2@gmail.com>
2023-06-15 21:43:11 -0500

    add function of elevate firts number with the second number

--- project/helloWorld.py
+++ project/helloWorld.py
@@ -1,5 +1,11 @@
+def elevate (num1 , num2):
+    return num1^num2
+
 def printAnyThing(text = None):
     print(text) if text != None else "No exist anything"
+    print(elevate(int(input()), int(input())))
+    
+
 
 
 printAnyThing(input())
