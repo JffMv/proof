5bfcee1914baa2d22e576763c8df0552c7eb09ea
Jefferson Mesa <mesajefer2@gmail.com>
2023-06-15 21:50:40 -0500

    update the function principal

--- project/helloWorld.py
+++ project/helloWorld.py
@@ -3,9 +3,9 @@ def elevate (num1 , num2):
 
 def printAnyThing(text = None):
     print(text) if text != None else "No exist anything"
-    print(elevate(int(input()), int(input())))
+    if text != None: print(elevate(int(input()), int(input())))
     
 
 
 
-printAnyThing(input())
+printAnyThing()
