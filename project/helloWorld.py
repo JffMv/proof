0064dec9031c947972b29dcb061a5a433b95d8e0
Jefferson Mesa <mesajefer2@gmail.com>
2023-06-27 23:02:00 -0500

    remember git

--- project/helloWorld.py
+++ project/helloWorld.py
@@ -2,9 +2,9 @@ def elevate (num1 , num2):
     return num1**num2
 
 def printAnyThing(text = None):
+    
     print(text) if text != None else "No exist anything"
     if text != None: print(elevate(int(input()), int(input())))
-    
 
 
 
